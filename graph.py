b29d6edc510363c1fb89a59154d489bb4b660212
JoshuaaCruz <joshua.cruz0520@gmail.com>
2026-03-20 16:57:22 -0300

    Ground work

---
+++ graph.py
@@ -0,0 +1,29 @@
+#  [Representa¸c˜ao] (2,0pts) Crie um tipo estruturado de dados ou uma classe que represente um grafo n˜ao-dirigido
+# e ponderado G(V, E, w), no qual V ´e o conjunto de v´ertices, E ´e o conjunto de arestas e w : E → R ´e a fun¸c˜ao que
+# mapeia o peso de cada aresta {u, v} ∈ E. As opera¸c˜oes/m´etodos contemplados para o grafo dever˜ao ser:
+# • qtdVertices(): retornr a quantidade de v´ertices;
+# • qtdArestas(): retorna a quantidade de arestas;
+# • grau(v): retorna o grau do v´ertice v;
+# • rotulo(v): retorna o r´otulo do v´ertice v;
+# • vizinhos(v): retorna os vizinhos do v´ertice v;
+# • haAresta(u, v): se {u, v} ∈ E, retorna verdadeiro; se n˜ao existir, retorna falso;
+# • peso(u, v): se {u, v} ∈ E, retorna o peso da aresta {u, v}; se n˜ao existir, retorna um valor infinito positivo1
+# ;
+# • ler(arquivo)2
+# : deve carregar um grafo a partir de um arquivo no formato especificado ao final deste documento.
+
+def qtdVertices():
+    pass
+
+def qtdArestas():
+    pass
+def grau(v):
+    pass
+def rotulo(v):
+    pass
+def vizinhos(v):
+    pass
+def haAresta(u, v):
+    pass
+def peso(u, v):
+    pass
